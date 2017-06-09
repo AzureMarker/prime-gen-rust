d6d0f2ad88e8c1748f590cc4c1ab573e2d3b83c2
Mcat12 <newtoncat12@yahoo.com>
2017-06-09 11:08:30 -0400

    Implement incremental sieve

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 use std::env;
 
 fn main() {
-    let target_primes: usize = env::args().nth(1).unwrap_or("1000000".to_string()).parse().expect("Invalid number");
-    let mut map = vec![false; 300_000_000];
-    let mut primes: Vec<usize> = Vec::with_capacity(target_primes);
-    primes.push(2);
-    let mut x = 3usize;
+    let target_primes = env::args().nth(1).unwrap_or("1000000".to_string()).parse().expect("Invalid number");
+    let mut primes = Vec::with_capacity(target_primes);
+    primes.push(2u32);
 
-    loop {
-        if map[x] {
-            x += 2;
-            continue;
-        }
+    for x in (3..).filter(|num| num % 2 == 1) {
+        let mut found = false;
 
-        primes.push(x);
-
-        if primes.len() == target_primes {
-            break;
-        }
-
-        let mut n = x*x;
-        loop {
-            if n >= map.len() {
+        let sqrt = (x as f64).sqrt() as u32;
+        for factor in primes.iter() {
+            if *factor > sqrt {
                 break;
             }
 
-            map[n] = true;
-            n += x;
+            if x % factor == 0 {
+                found = true;
+                break;
+            }
         }
 
-        x += 2;
+        if !found {
+            primes.push(x);
+
+            if primes.len() == target_primes {
+                break;
+            }
+        }
+        else {
+            continue;
+        }
     }
 
-    println!("The #{} prime: {}", target_primes, primes.last().unwrap());
+    println!("{} is the #{} prime", primes.last().unwrap(), primes.len());
 }
